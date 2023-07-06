91da387f5acc693d3793f6d0a81cf9fe6bc4a239
DesignedByQ <henry.o@hotmail.co.uk>
2023-07-06 20:24:35 +0100

    Initial Push

---
+++ Accounts/src/main/java/com/techprj/Accounts/BankingAppAccApplication.java
@@ -0,0 +1,13 @@
+package com.techprj.Accounts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingAppAccApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingAppAccApplication.class, args);
+	}
+
+}
---
+++ Accounts/src/test/java/com/techprj/Accounts/BankingAppAccApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techprj.Accounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingAppAccApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
